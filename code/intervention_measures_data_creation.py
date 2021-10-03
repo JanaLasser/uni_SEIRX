cd58709280085c698d22e39371467ec5d3bb337e
Jana <jana.lasser@ds.mpg.de>
2021-10-03 10:35:41 +0200

    updated to work with new data creation functions

--- code/intervention_measures_data_creation.py
+++ code/intervention_measures_data_creation.py
@@ -12,6 +12,7 @@ if N_runs == 1:
 	testing = True
 
 screening_params = pd.read_csv(join('screening_params', 'measure_packages.csv'))
+mode = 'intervention_screening'
 
 params = [(N_runs, 
            row['u_mask'],
@@ -25,14 +26,13 @@ print('there are {} different parameter combinations'.format(len(params)))
 
 
 contact_network_src = '../data/networks'
-dst = '../data/simulation_results/ensembles'
+dst = '../data/simulation_results/ensembles_{}'.format(mode)
 
-results = pd.DataFrame()
 for p in params:
     N_runs, u_mask, l_mask, u_vaccination_ratio,\
     l_vaccination_ratio, presence_fraction = p
     
-    dcf.run_ensemble(N_runs, contact_network_src, dst, 
+    dcf.run_ensemble(mode, N_runs, contact_network_src, dst, 
             u_mask=u_mask, l_mask=l_mask,
             u_vaccination_ratio=u_vaccination_ratio,
             l_vaccination_ratio=l_vaccination_ratio,
