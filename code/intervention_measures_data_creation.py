66c3ddd8f48426adba6a8584b55ef90b1703b84d
Jana <jana.lasser@ds.mpg.de>
2021-11-07 21:00:00 +0100

    fixed bug

--- code/intervention_measures_data_creation.py
+++ code/intervention_measures_data_creation.py
@@ -33,6 +33,12 @@ for p in params:
     N_runs, u_mask, l_mask, u_vaccination_ratio,\
     l_vaccination_ratio, presence_fraction, contact_network_type = p
     
+    if presence_fraction == 1:
+        presence_fraction = 1.0
+        
+    if presence_fraction == 'overbooked':
+        continue
+    
     dst = '../data/simulation_results/ensembles_{}_{}'\
         .format(mode, contact_network_type)
     
