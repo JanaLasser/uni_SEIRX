7b6bcbb8e4fb9f3cbc4d91c1af2ef384bf7e39b9
Jana <jana.lasser@ds.mpg.de>
2021-11-07 13:15:57 +0100

    added differentiation into all students, only TU Graz and only NaWi for no measures and TTI only simulation runs

--- code/TTI_data_creation.py
+++ code/TTI_data_creation.py
@@ -13,6 +13,26 @@ if N_runs == 1:
 
 mode = 'TTI'
 contact_network_src = '../data/networks'
-dst = '../data/simulation_results/ensembles_{}'.format(mode)
+
+params = [(N_runs, 
+           False,
+           False, 
+           0.0,
+           0.0,
+           'overbooked',
+           contact_network_type)
+           for contact_network_type in ['all', 'TU', 'NaWi']]
+
+for p in params:
+    N_runs, u_mask, l_mask, u_vaccination_ratio,\
+    l_vaccination_ratio, presence_fraction, contact_network_type = p
     
-dcf.run_ensemble(mode, N_runs, contact_network_src, dst, testing)
+    dst = '../data/simulation_results/ensembles_{}_{}'\
+        .format(mode, contact_network_type)
+    
+    dcf.run_ensemble(mode, N_runs, contact_network_src, 
+            contact_network_type, dst, 
+            u_mask=u_mask, l_mask=l_mask,
+            u_vaccination_ratio=u_vaccination_ratio,
+            l_vaccination_ratio=l_vaccination_ratio,
+            presence_fraction=presence_fraction, testing=testing)
